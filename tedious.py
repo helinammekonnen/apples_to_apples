d43a4f8204da949a7f9321c9253abccbb972017d
helinammekonnen <31739113+helinammekonnen@users.noreply.github.com>
2018-01-11 12:18:49 -0500

    Update tedious.py

--- tedious.py
+++ tedious.py
@@ -1,4 +1,4 @@
 green_list = ["Absurd", "Addictive", "Adorable", "Aged", "American", "Ancient", "Animated", "Annoying", "Appetizing", "Arrogant", "Awesome", 
               "Awkward", "Believable", "Bogus", "Boisterous", "Bold", "Boring", "Bright", "Brilliant", "Busy", "Calm", "Casual", "Charismatic", 
               "Charming", "Cheesy", "Chewy", "Chunky", "Classic", "Clean", "Clueless", "Cold", "Colorful", "Comfortable", "Comical", "Complicated", 
-              "Confused", "Cool", "Corrupt", "Cosmic", "Cosmopolitan", ""
+              "Confused", "Cool", "Corrupt", "Cosmic", "Cosmopolitan"]
